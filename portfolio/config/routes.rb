3be834ebe301d38db9c5ddb36689e4a51890cb1f
Marty Kovach <tyedye105@yahoo.com>
2017-03-03 10:41:44 -0800

    create users show, and index to be userd for about me, and project display.

--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :landing, :only =>'index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:show, :index]
   root "landing#index"
 end
