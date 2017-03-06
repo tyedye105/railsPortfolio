bfc383cf99c883d577c30459207537846d8b8c66
Marty Kovach <tyedye105@yahoo.com>
2017-03-05 22:01:08 -0800

    re-factor routes to add posts to be viewed by anyone, but only edited to allow admins to make new posts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :landing, :only =>'index'
   resources :users, :only => [:show, :index]
-  resources :posts
+  resources :users do
+    resources :posts, :only =>[:new, :create, :edit, :update, :destroy]
+  end
+  resources :posts, :only => [:index, :show]
   resources :projects, :only => 'index'
   root "landing#index"
 end
