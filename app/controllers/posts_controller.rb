bdea181bc56950bb427892aa4881cea4a861078b
Marty Kovach <tyedye105@yahoo.com>
2017-03-05 23:03:04 -0800

    add add post function.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @user = current_user
     @posts = Post.all
   end
   def  show
@@ -7,11 +8,27 @@ class PostsController < ApplicationController
   end
   def new
     @user = current_user
-      @user.is_admin?
-      @post= Post.new
+      if @user.is_admin === true
+      @post = @user.posts.new
     else
       flash[:alert] = "Admins only"
       redirect_to posts_path
+    end
   end
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+    if @post.save
+        flash[:notice] = "You have added new blog post!"
+        redirect_to posts_path
+      else
+        flash[:alert] = "Woops"
+        render :new
+      end
+    end
 
+    private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
